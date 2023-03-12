cf5bdbf19d6c6b2b056304107c4536d70763882c
ldrennan279 <leedrennan279@gmail.com>
2023-03-12 18:57:55 +0000

    1st Submit

---
+++ dogDate.js
@@ -0,0 +1,268 @@
+
+
+const dogData = [
+    {
+        name: "funnyDog1", 
+        imageName: "images/funnyDog1.jpg",
+        type: "Hungry",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog2", 
+        imageName: "images/funnyDog2.jpg",
+        type: "Well hello!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog3", 
+        imageName: "images/funnyDog3.jpg",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog4", 
+        imageName: "images/funnyDog4.jpg",
+        type: "Well hello!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog5", 
+        imageName: "images/funnyDog5.jpg",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog6", 
+        imageName: "images/funnyDog6.jpg",
+        type: "HAHAHAHA!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog7", 
+        imageName: "images/funnyDog7.jpg",
+        type: "HAHAHAHA!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog8", 
+        imageName: "images/funnyDog8.jpg",
+        type: "HAHAHAHA!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog9", 
+        imageName: "images/funnyDog9.jpg",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog10", 
+        imageName: "images/funnyDog10.jpg",
+        type: "Hungry",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog11", 
+        imageName: "images/funnyDog11.jpg",
+        type: "Kiss ma Butt!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog12", 
+        imageName: "images/funnyDog12.jpg",
+        type: "Well hello!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog13", 
+        imageName: "images/funnyDog13.jpg",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog14", 
+        imageName: "images/funnyDog14.jpg",
+        type: "Well hello!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog15", 
+        imageName: "images/funnyDog15.jpg",
+        type: "Ready to go!!", 
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog16", 
+        imageName: "images/funnyDog16.jpg",
+        type: "Relaxed",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog17", 
+        imageName: "images/funnyDog17.jpg",
+        type: "Happy",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog18", 
+        imageName: "images/funnyDog18.jpg",
+        type: "Ready to go!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog19", 
+        imageName: "images/funnyDog19.jpg",
+        type: "Party!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog20", 
+        imageName: "images/funnyDog20.jpg",
+        type: "Party!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog21", 
+        imageName: "images/funnyDog21.jpg",
+        type: "Happy",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog22", 
+        imageName: "images/funnyDog22.jpg",
+        type: "Happy",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog23", 
+        imageName: "images/funnyDog23.jpg",
+        type: "Ready to go!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnyDog24", 
+        imageName: "images/funnyDog24.jpg",
+        type: "Ready to go!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog1", 
+        imageName: "images/funnygifDog1.gif",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog2", 
+        imageName: "images/funnygifDog2.gif",
+        type: "Ready to go!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog3", 
+        imageName: "images/funnygifDog3.gif",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog4", 
+        imageName: "images/funnygifDog4.gif",
+        type: "Kiss ma Butt!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog5", 
+        imageName: "images/funnygifDog5.gif",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog6", 
+        imageName: "images/funnygifDog6.gif",
+        type: "Party!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog7", 
+        imageName: "images/funnygifDog7.gif",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog8", 
+        imageName: "images/funnygifDog8.gif",
+        type: "Ready to go!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog9", 
+        imageName: "images/funnygifDog9.gif",
+        type: "Relaxed",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog10", 
+        imageName: "images/funnygifDog10.gif",
+        type: "Kiss ma Butt!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog11", 
+        imageName: "images/funnygifDog11.gif",
+        type: "Relaxed",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog12", 
+        imageName: "images/funnygifDog12.gif",
+        type: "Well hello!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog13", 
+        imageName: "images/funnygifDog13.gif",
+        type: "Relaxed",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog14", 
+        imageName: "images/funnygifDog14.gif",
+        type: "Hungry",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog15", 
+        imageName: "images/funnygifDog15.gif",
+        type: "Party!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog16", 
+        imageName: "images/funnygifDog16.gif",
+        type: "HAHAHAHA!!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog17", 
+        imageName: "images/funnygifDog17.gif",
+        type: "What you looking at??",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog18", 
+        imageName: "images/funnygifDog18.gif",
+        type: "Kiss ma Butt!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog19", 
+        imageName: "images/funnygifDog19.gif",
+        type: "Party!",
+        id: "dog-pic"
+    },
+    {
+        name: "funnygifDog20", 
+        imageName: "images/funnygifDog20.gif",
+        type: "Happy",
+        id: "dog-pic"
+    }
+]
