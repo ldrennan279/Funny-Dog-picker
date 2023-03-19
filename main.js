1123c5cb921bdef2d409af421af54df55cad7e03
ldrennan279 <leedrennan279@gmail.com>
2023-03-19 11:55:49 +0000

    3rd submit
    Working on .includes & .filter to remove duplicates.

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-import { dogInfo } from "/dogDate.js"
+import { dogInfo } from "/dogData.js"
 
 const dogList = document.getElementById("dogpick")
 
+function getEmotions(dogs){
+    const emotionArray =[]
+    for(let dog of dogs){
+        for(let emotion of dog.type){
+            if(emotionArray.includes(dog.type)){
+                
+            } else if(!emotionArray.includes(dog.type)){
+                emotionArray.push(emotion)
+            }
+        }
+    }
+    return emotionArray
+}
+
+function renderDogMoods(dogs){
+    let displayBtn = ``
+    const moods = getEmotions(dogs)
+    for (let mood of moods){
+        displayBtn += `
+            <p class="button">
+                <span class="btnTextStyle">${mood}</span>
+            </p>`
+    }
+    dogList.innerHTML = displayBtn
+}
+
+renderDogMoods(dogInfo)
