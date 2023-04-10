87cea74684875a558efabf43f1e06db7c6a5429b
ldrennan279 <leedrennan279@gmail.com>
2023-04-10 10:17:30 +0100

    5th submit

--- dogData.js
+++ dogData.js
@@ -5,265 +5,309 @@ export const dogInfo = [
         name: "funnyDog1", 
         imageName: "images/funnyDog1.jpg",
         type: ["Hungry"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog2", 
         imageName: "images/funnyDog2.jpg",
         type: ["Well hello!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog3", 
         imageName: "images/funnyDog3.jpg",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog4", 
         imageName: "images/funnyDog4.jpg",
         type: ["Well hello!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog5", 
         imageName: "images/funnyDog5.jpg",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog6", 
         imageName: "images/funnyDog6.jpg",
         type: ["HAHAHAHA!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog7", 
         imageName: "images/funnyDog7.jpg",
         type: ["HAHAHAHA!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog8", 
         imageName: "images/funnyDog8.jpg",
         type: ["HAHAHAHA!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog9", 
         imageName: "images/funnyDog9.jpg",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog10", 
         imageName: "images/funnyDog10.jpg",
         type: ["Hungry"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog11", 
         imageName: "images/funnyDog11.jpg",
         type: ["Kiss ma Butt!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog12", 
         imageName: "images/funnyDog12.jpg",
         type: ["Well hello!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog13", 
         imageName: "images/funnyDog13.jpg",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog14", 
         imageName: "images/funnyDog14.jpg",
         type: ["Well hello!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog15", 
         imageName: "images/funnyDog15.jpg",
         type: ["Ready to go!!"], 
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog16", 
         imageName: "images/funnyDog16.jpg",
         type: ["Relaxed"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog17", 
         imageName: "images/funnyDog17.jpg",
         type: ["Happy"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog18", 
         imageName: "images/funnyDog18.jpg",
         type: ["Ready to go!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog19", 
         imageName: "images/funnyDog19.jpg",
         type: ["Party!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog20", 
         imageName: "images/funnyDog20.jpg",
         type: ["Party!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog21", 
         imageName: "images/funnyDog21.jpg",
         type: ["Happy"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog22", 
         imageName: "images/funnyDog22.jpg",
         type: ["Happy"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog23", 
         imageName: "images/funnyDog23.jpg",
         type: ["Ready to go!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnyDog24", 
         imageName: "images/funnyDog24.jpg",
         type: ["Ready to go!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: false
     },
     {
         name: "funnygifDog1", 
         imageName: "images/funnygifDog1.gif",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog2", 
         imageName: "images/funnygifDog2.gif",
         type: ["Ready to go!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog3", 
         imageName: "images/funnygifDog3.gif",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog4", 
         imageName: "images/funnygifDog4.gif",
         type: ["Kiss ma Butt!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog5", 
         imageName: "images/funnygifDog5.gif",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog6", 
         imageName: "images/funnygifDog6.gif",
         type: ["Party!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog7", 
         imageName: "images/funnygifDog7.gif",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog8", 
         imageName: "images/funnygifDog8.gif",
         type: ["Ready to go!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog9", 
         imageName: "images/funnygifDog9.gif",
         type: ["Relaxed"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog10", 
         imageName: "images/funnygifDog10.gif",
         type: ["Kiss ma Butt!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog11", 
         imageName: "images/funnygifDog11.gif",
         type: ["Relaxed"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog12", 
         imageName: "images/funnygifDog12.gif",
         type: ["Well hello!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog13", 
         imageName: "images/funnygifDog13.gif",
         type: ["Relaxed"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog14", 
         imageName: "images/funnygifDog14.gif",
         type: ["Hungry"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog15", 
         imageName: "images/funnygifDog15.gif",
         type: ["Party!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog16", 
         imageName: "images/funnygifDog16.gif",
         type: ["HAHAHAHA!!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog17", 
         imageName: "images/funnygifDog17.gif",
         type: ["What you looking at??"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog18", 
         imageName: "images/funnygifDog18.gif",
         type: ["Kiss ma Butt!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog19", 
         imageName: "images/funnygifDog19.gif",
         type: ["Party!"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     },
     {
         name: "funnygifDog20", 
         imageName: "images/funnygifDog20.gif",
         type: ["Happy"],
-        id: "dog-pic"
+        id: "dog-pic",
+        isGif: true
     }
 ]
 
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { dogInfo } from "/dogData.js"
 
-const dogList = document.getElementById("dogpick")
+const dogList = document.querySelector(".dogpick")
 const model = document.getElementById("model")
+const modelInner = document.getElementById("model-inner")
+let selectedMood = ""
 
 function getEmotions(dogs){
     const emotionArray =[]
@@ -22,18 +24,54 @@ function renderDogMoods(dogs){
     const moods = getEmotions(dogs)
     for (let mood of moods){
         displayBtn += `
-            <p class="button" id="${mood}">
-                <span class="btnTextStyle" id="${mood}">
+            <button type="button" class="button" id="${mood}">
                     ${mood}
-                </span>
-            </p>`
+            </button>
+            `
     }
     dogList.innerHTML = displayBtn
 }
 
 renderDogMoods(dogInfo)
 
-dogList.addEventListener("click", function(e){
-    console.log(e.target.id)
-})
 
+document.addEventListener("click", function(e){
+    if(e.target.className === "button"){ 
+        selectedMood = e.target.id
+        displayModel() 
+    }
+})
+   
+
+// fix function to push only gifs images to array if radio is checked
+
+
+
+function getDogImage(dogs){
+    const dogImageArray = []
+    for (let dog of dogs){
+        for (let emotion of dog.type)
+        if(emotion === selectedMood){
+            dogImageArray.push(dog.imageName)
+        }
+    }
+    const randomImage = Math.floor(Math.random() * dogImageArray.length)
+    return dogImageArray[randomImage]
+}
+
+function displayModel(){
+    const dogImage = getDogImage(dogInfo)
+    modelInner.innerHTML = `
+    <h2 class="close-btn" id="close-btn">X</h2>
+    <img src="/${dogImage}" width="300px">
+    `
+    model.style.display = "block"
+    
+}
+
+document.addEventListener('click', (e)=>{
+    if(e.target.id === "close-btn"){
+        model.style.display = "none"
+    }
+    
+})
